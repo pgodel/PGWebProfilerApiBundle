336b8f9b59cac3597808e463285283eacc8e8f50
Pablo Godel <pgodel@gmail.com>
2014-08-07 23:37:56 -0400

    initial structure

---
+++ Controller/WebProfilerApiController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace PG\Bundle\WebProfilerApiBundle\Controller;
+
+use FOS\RestBundle\Util\Codes;
+
+use FOS\RestBundle\Controller\Annotations;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Request\ParamFetcherInterface;
+use FOS\RestBundle\View\RouteRedirectView;
+
+use FOS\RestBundle\View\View;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class WebProfilerApiController extends FOSRestController
+{
+    public function indexAction()
+    {
+        return $this->render('PGWebProfilerApiBundle:Default:index.html.twig', array());
+    }
+
+    /**
+     * List all tokens.
+     *
+     * @Annotations\QueryParam(name="offset", requirements="\d+", nullable=true, description="Offset from which to start listing tokens.")
+     * @Annotations\QueryParam(name="limit", requirements="\d+", default="5", description="How many tokens to return.")
+     *
+     * @Annotations\View()
+     *
+     * @param Request               $request      the request object
+     * @param ParamFetcherInterface $paramFetcher param fetcher service
+     *
+     * @return array
+     */
+    public function getTokensAction(Request $request, ParamFetcherInterface $paramFetcher)
+    {
+        $session = $request->getSession();
+
+        $offset = $paramFetcher->get('offset');
+        $start = null == $offset ? 0 : $offset + 1;
+        $limit = $paramFetcher->get('limit');
+
+
+        return array();
+
+    }
+
+    /**
+     * Get a token.
+     * 
+     * @Annotations\View(templateVar="token")
+     *
+     * @param Request $request the request object
+     * @param int     $token      the token id
+     *
+     * @return array
+     *
+     * @throws NotFoundHttpException when note not exist
+     */
+    public function getTokenAction(Request $request, $token)
+    {
+
+        $tokens = [$token => $token];
+
+        if (!isset($tokens[$token])) {
+            throw $this->createNotFoundException("Token does not exist.");
+        }
+
+        $view = new View($tokens[$token]);
+
+        return $view;
+    }
+}
---
+++ PGWebProfilerApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PG\Bundle\WebProfilerApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PGWebProfilerApiBundle extends Bundle
+{
+}
